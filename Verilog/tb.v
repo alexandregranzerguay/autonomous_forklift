76007bf2d12931d788cb680ebddd6368bcc6fea9
RWajs <13rjw15@queensu.ca>
2018-11-30 15:57:17 -0500

    Add stepper_basic
    Added stepper basic file and changed testbench to test stepper logic

--- Verilog/tb.v
+++ Verilog/tb.v
@@ -2,9 +2,7 @@
 module test_bench;
 
 	reg clk, reset_n;
-	wire [5:0] state_out;
-	wire [5:0] state_out_div;
-	wire clk_div;
+	wire pwm_out;
 	always begin
 		# 0.5
 		clk <= !clk;
@@ -19,10 +17,6 @@ module test_bench;
 		reset_n <= 1;
 	end
 	
-	clock_divider ck_div(clk, reset_n, clk_div);
-	
-	stepper_control stepper(clk_div, reset_n, state_out);
-	
-	stepper_control_divided stepper_div(clk, reset_n, state_out_div);
+	servo_basic servo(clk, reset_n, pwm_out);
 	
 endmodule
