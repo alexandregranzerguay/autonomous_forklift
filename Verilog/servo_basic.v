722017ca303bdd84e523457c329dada61961f32f
RWajs <13rjw15@queensu.ca>
2019-01-11 12:39:01 -0500

    Servo basic fix

--- Verilog/servo_basic.v
+++ Verilog/servo_basic.v
@@ -2,11 +2,11 @@
 module servo_basic(input clock_clk, input reset_low, output reg pwm_out, output high, output gnd);
 	reg [31:0] count;
 	
-	localparam totalTime = 10;
-	localparam halfTime = 1;
+	localparam totalTime = 500000000;
+	localparam halfTime = 250000000;
 	
 	// Move FSM when clock cycles
-	always @(clock_clk or reset_low) begin
+	always @(posedge clock_clk or negedge reset_low) begin
 		if (reset_low == 0) begin
 			count <= 0;
 		end
